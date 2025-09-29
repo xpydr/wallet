8aa9838c6dc47cf72218f6839b8eb9a2a30ca8e2
xpydr <purirohan62@gmail.com>
2025-09-29 16:23:01 -0400

    added Nuxt

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+    <nuxt-page />
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-screen bg-green-100 font-mono">
+    <div class="flex justify-center items-center border">
+      <div class="my-32 shadow">
+        <div class="border m-6 p-6 shadow hover:shadow-xl hover:cursor-pointer hover:bg-red-100">
+          Create
+        </div>
+        <hr class="mx-2">
+        <div class="border m-6 p-6 shadow hover:shadow-xl hover:cursor-pointer hover:bg-red-100">
+          Import
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script> 
