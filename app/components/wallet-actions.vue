138b97926336deb21d898906a29431ed3a5a6ffa
xpydr <purirohan62@gmail.com>
2025-10-01 00:37:12 -0400

    add qrcode ui

--- app/components/wallet-actions.vue
+++ app/components/wallet-actions.vue
@@ -1,29 +1,46 @@
 <template>
-    <div class="flex justify-around">
+    <div class="flex flex-col justify-center items-center gap-16">
         <div>
-            <button class="border p-4">Deposit</button>
+            <button @click="isDeposit = true" class="border p-4">Deposit</button>
+            <button @click="isDeposit = false" class="border p-4">Withdraw</button>
+
         </div>
-        <div>
-            <button class="border p-4">Withdraw</button>
+        <div v-if="isDeposit">
+            <qrcode-vue :value="hexInput" :size="200" level="H" render-as="svg" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import QrcodeVue from 'qrcode.vue';
+
+// This will be a prop
+const hexInput = ref<string>('0x294897f04cfA7de68aE6c76553CE4Cb520261624')
+const qrError = ref<string>('')
+
+const isDeposit = ref<boolean>(true)
+
+watch(hexInput, (newValue) => {
+    if (newValue && !/^[0-9A-Fa-f]*$/.test(newValue)) {
+        qrError.value = 'Please enter a valid hexadecimal string'
+    } else {
+        qrError.value = ''
+    }
+})
 </script>
 
 <style scoped>
 button {
-	cursor: pointer;
-	transition: background-color 0.2s;
+    cursor: pointer;
+    transition: background-color 0.2s;
 }
 
 button:hover {
-	background-color: #f0f0f0;
+    background-color: #f0f0f0;
 }
 
 button:disabled {
-	cursor: not-allowed;
-	opacity: 0.5;
+    cursor: not-allowed;
+    opacity: 0.5;
 }
 </style>
