6b12884b18f38586ead261c11a18928c35420a53
xpydr <purirohan62@gmail.com>
2025-10-07 18:13:02 -0400

    add styling on new + load buttons

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,17 @@
 <template>
-	<div class="m-16">
-		<div class="flex flex-col justify-center items-center bg-white">
-			<h1 class="text-4xl text-black m-16">
-				Wallet
-			</h1>
-			<div class="text-xl flex justify-around w-full">
-				<p class="hover:cursor-pointer">New</p>
-				<p class="hover:cursor-pointer">Load</p>
-				
-			</div>
-	
-			<div>
-				
-			</div>
-		</div>
-	</div>
+  <div class="m-16">
+    <div class="flex flex-col justify-center items-center bg-white">
+      <h1 class="text-4xl text-black m-16">
+        Wallet
+      </h1>
+      <div class="text-xl flex justify-around w-full">
+        <router-link to="wallet"
+          class="hover:shadow-lg hover:bg-cyan-300 rounded-xl p-4 hover:shadow-cyan-300 transition">New</router-link>
+        <router-link to="wallet"
+          class="hover:shadow-lg hover:bg-cyan-300 rounded-xl p-4 hover:shadow-cyan-300 transition">Load</router-link>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 </script>
