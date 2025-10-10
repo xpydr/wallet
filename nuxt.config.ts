eed179f6c57bcca705e2f4d8539a15d67240b77d
xpydr <purirohan62@gmail.com>
2025-10-09 20:09:18 -0400

    add dep: pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/icon', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
       alchemyApiKey: process.env.API_KEY,
