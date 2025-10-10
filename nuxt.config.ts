3bab1ed58aacd4e16af339e4ad7cedd68150775c
xpydr <purirohan62@gmail.com>
2025-10-10 08:45:30 -0400

    add: pinia plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  plugins: ['~/plugins/pinia.ts'],
   runtimeConfig: {
     public: {
       alchemyApiKey: process.env.API_KEY,
