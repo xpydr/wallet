8af27a38e0f0f27cfce488ea40ad7508285f6e97
xpydr <purirohan62@gmail.com>
2025-10-13 17:01:44 -0400

    add: getBalance action use state variable address if none provided

--- app/stores/wallet.ts
+++ app/stores/wallet.ts
@@ -4,15 +4,53 @@ import { ethers } from 'ethers';
 import { fetchBalance } from '~/services/walletService';
 import type { WalletMode } from '~/types';
 
-export const useWalletStore = defineStore('walletStore', {
-    state: () => ({
-        address: ref<string>(''),
-        mnemonic: ref<string>(''),
-        balance: ref<string>(''),
-        keystore: ref<string>('')
+interface WalletState {
+    address: Ref<string>;
+    mnemonic: Ref<string>;
+    balance: Ref<string>;
+    keystore: Ref<string | null>;
+    password: Ref<string | null>;
+}
+interface WalletActions {
+    createWallet(wordCount: WalletMode, password: string): Promise<{ success: boolean }>;
+    recoverWallet(password: string): Promise<{ success: boolean }>;
+    getBalance(address: string): Promise<{ balance: string }>;
+    initUser(password: string): Promise<{ success: boolean }>;
+    clearPassword(): void;
+}
+
+export const useWalletStore = defineStore<
+    'walletStore', WalletState, {
+        getBalance():
+            string
+    },
+    WalletActions
+>('walletStore', {
+    state: (): WalletState => ({
+        address: ref(''),
+        mnemonic: ref(''),
+        balance: ref(''),
+        keystore: ref(''),
+        password: ref(''),
     }),
     actions: {
-        async createWallet(wordCount: WalletMode): Promise<{ success: boolean }> {
+        async initUser(password: string): Promise<{ success: boolean }> {
+            try {
+                this.password = password
+                this.keystore = localStorage.getItem('walletKeystore') || null
+                if (!this.keystore) {
+                    await this.createWallet(12, password)
+                } else {
+                    await this.recoverWallet(password)
+                }
+                return { success: true }
+            } catch (err: any) {
+                console.error(err)
+            }
+            return { success: false };
+        },
+
+        async createWallet(wordCount: WalletMode, password: string): Promise<{ success: boolean }> {
             try {
                 const entropyBits: Record<number, number> = {
                     12: 128,
@@ -36,7 +74,6 @@ export const useWalletStore = defineStore('walletStore', {
                 this.mnemonic = mnemonic.phrase;
                 this.balance = await fetchBalance(wallet.address);
 
-                const password = ''; // user set password
                 const jsonKeystore = await wallet.encrypt(password);
                 this.keystore = jsonKeystore;
                 localStorage.setItem('walletKeystore', jsonKeystore);
@@ -51,7 +88,7 @@ export const useWalletStore = defineStore('walletStore', {
             try {
                 const jsonKeystore = this.keystore || localStorage.getItem('walletKeystore');
                 if (!jsonKeystore) throw new Error('No keystore found');
-                
+
                 const wallet = await ethers.Wallet.fromEncryptedJson(jsonKeystore, password);
 
                 this.address = wallet.address;
@@ -64,14 +101,19 @@ export const useWalletStore = defineStore('walletStore', {
                 return { success: false };
             }
         },
+        async getBalance(address?: string): Promise<{ balance: string }> {
+            try {
+                address ? null : address = this.address;
+                const bal: string = await fetchBalance(address);
+                if (!address) this.balance = bal;
+                return { balance: bal };
+            } catch (err: any) {
+                console.error(err);
+                return { balance: 'error' };
+            }
+        },
+        clearPassword() {
+            this.password = '';
+        },
     },
-    async getBalance(address: string): Promise<{ success: boolean }> {
-        try {
-            this.balance = await fetchBalance(address);
-            return { success: true }
-        } catch (err: any) {
-            console.error(err);
-            return { success: false }
-        }
-    }
 });
