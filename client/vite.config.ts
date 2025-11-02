2b4ebce7bad69da270b483308bcd988ce37ff2e9
xpydr <purirohan62@gmail.com>
2025-11-02 17:47:25 -0500

    add: tailwind plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
@@ -8,5 +9,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss()
   ],
 })
