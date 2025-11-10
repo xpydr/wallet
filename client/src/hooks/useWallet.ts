fb64e540df398b2311fc5a7eac056b751024b56b
xpydr <purirohan62@gmail.com>
2025-11-10 11:11:36 -0500

    wip: start custom hooks file

---
+++ client/src/hooks/useWallet.ts
@@ -0,0 +1,18 @@
+
+export async function useInitUser() {
+}
+
+export async function useCreateWallet() {
+}
+
+export async function useRecoverWallet() {
+}
+
+export async function useGetBalance() {
+}
+
+export async function useSendTx() {
+}
+
+export async function useClearSession() {
+}
