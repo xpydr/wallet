bd58d60a272405b48cf69c32c1631bdb2e64248a
xpydr <purirohan62@gmail.com>
2025-11-21 10:51:26 -0500

    feat(api): add get-tx-data endpoint with fee/nonce retrieval and gwei formatting helper
    get-tx-data accepts address param and retrieves nonce
    
    endpoint calls helper function fetchFeeData to retrieve feeData using ethers v6

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const {body, param, query, validationResult} = require('express-validator');
+const { body, param, query, validationResult } = require('express-validator');
 
 const ethers = require('ethers');
 const dotenv = require('dotenv');
@@ -13,14 +13,14 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
-app.listen(port, () => {console.log(`Server running on port ${port}`)});
+app.listen(port, () => { console.log(`Server running on port ${port}`) });
 
 const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${apiKey}`);
 
 const validate = (req, res, next) => {
   const errors = validationResult(req.body);
   if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
+    return res.status(400).json({ errors: errors.array() });
   }
   next();
 }
@@ -33,9 +33,11 @@ app.post('/api/v1/get-balance',
   async (req, res) => { // get native balance by address
     try {
       const balance = await provider.getBalance(req.body.address);
-      res.status(201).json({message: "Balance fetched successfully.", balance: ethers.formatEther(balance)});
+      res.status(200).json({ message: "Balance fetched successfully.", balance: ethers.formatEther(balance) });
+      console.log(res)
     } catch (err) {
-      res.status(500).json({message: err.message});
+      console.log(err)
+      res.status(500).json({ message: err.message });
     }
   }
 );
@@ -47,10 +49,62 @@ app.post('/api/v1/send-tx',
   validate,
   async (req, res) => { // broadcast signed tx (hex string)
     try {
-      const response = await provider.broadcastTransaction(req.body.txSigned);
-      res.status(201).json({message: "Balance fetched successfully.", hash: response.hash});
-    } catch (err) {
-      res.status(500).json({message: err.message});
+      const txResponse = await provider.broadcastTransaction(req.body.txSigned);
+      if (!txResponse || !txResponse.hash) {
+        return res.status(500).json({
+          message: "Transaction submission failed: no hash returned"
+        });
+      }
+
+      res.status(202).json({
+        message: "Transaction submission successful",
+        hash: txResponse.hash
+      });
+
+    } catch (error) {
+      console.error("Error broadcasting transaction:", error);
+      res.status(500).json({
+        message: "Transaction submission failed",
+        error: error.message
+      });
     }
   }
 );
+
+app.post('api/v1/get-tx-data',
+  [
+    body('address').isString().trim().notEmpty(),
+  ],
+  validate,
+  async (req, res) => {
+    try {
+      const nonce = await provider.getTransactionCount(req.body.address);
+      const feeData = await fetchFeeData();
+      res.status(200).json({
+        message: "Data retrieval successful",
+        nonce,
+        feeData,
+      });
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: err.message, error: err });
+    }
+  }
+);
+
+async function fetchFeeData() {
+  try {
+    const feeData = await provider.getFeeData();
+    if (!feeData.maxFeePerGas || !feeData.maxPriorityFeePerGas) throw new Error("Error fetching feeData");
+    
+    feeData.maxFeePerGas = ethers.formatUnits(feeData.maxFeePerGas, "gwei");
+    feeData.maxPriorityFeePerGas = ethers.formatUnits(feeData.maxPriorityFeePerGas, "gwei");
+    feeData.gasPrice = ethers.formatUnits(feeData.gasPrice, "gwei");
+
+    return feeData;
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message, error: err });
+  }
+}
