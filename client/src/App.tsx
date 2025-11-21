ee700f5719effba6232d629ac5b9053f406b236f
xpydr <purirohan62@gmail.com>
2025-11-21 12:17:41 -0500

    feat(client): add App.tsx with Toaster
    returns Toaster from react-hot-toast
    
    returns AppProviders (WalletContext & AuthContext) wrapping Wallet page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+
+import '@/App.css';
+import Wallet from '@/pages/Wallet';
+import { AppProviders } from '@/context/AppProviders';
+import { Toaster } from "react-hot-toast";
 
 function App() {
 
   return (
     <>
+      <Toaster />
+      <div className='font-mono min-h-screen bg-black flex justify-center items-center'>
+        <AppProviders>
+          <Wallet />
+        </AppProviders>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
