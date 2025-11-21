1cb25d8efe1374fa28d9355f2179f3302fdbdec1
xpydr <purirohan62@gmail.com>
2025-11-21 10:59:01 -0500

    feat(types): add Wallet and TxBody types

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,2 +1,19 @@
 
 export type WalletMode = 12 | 15 | 18 | 21 | 24;
+
+export type Wallet = {
+  address: string;
+  balance: number;
+  mnemonic: string
+} | null;
+
+export type TxBody = {
+  to: string;
+  value: bigint; // ether amount
+  nonce: bigint;
+  gasLimit: bigint;
+  gasPrice: bigint;
+  chainId: bigint;
+  data: string;
+  type: number;
+}
