45955fc381b6388e7f0a25335a1307391de00f5a
xpydr <purirohan62@gmail.com>
2025-11-21 11:45:19 -0500

    feat(wallet): add Wallet page as entrypoint if authorized
    if keystore is unlocked, user will be directed to their wallet
    
    if keystore is locked, user must enter a password to unlock keystore or create new password for a new wallet

--- client/src/pages/Wallet.tsx
+++ client/src/pages/Wallet.tsx
@@ -1,13 +1,15 @@
 
-import AuthKeystore from "../components/wallet/AuthKeystore";
-import WalletApp from "../components/wallet/WalletApp";
+import AuthKeystore from "@/components/wallet/AuthKeystore";
+import WalletApp from "@/components/wallet/WalletApp";
+import { useAuth } from "@/context/AuthContext";
 
 function Wallet() {
-  const isLoggedIn: boolean = true;
-  return(
-    <div className="flex justify-center items-center pb-2 dark:bg-black dark:text-white transition">
-      <div className="border text-sm p-4">
-        {isLoggedIn ? <WalletApp /> : <AuthKeystore />}
+  const { isAuth } = useAuth();
+
+  return (
+    <div className="flex justify-center items-center dark:bg-black dark:text-white transition">
+      <div className="border max-w-5xl text-sm primary-container">
+        {isAuth ? <WalletApp /> : <AuthKeystore />}
       </div>
     </div>
   );
