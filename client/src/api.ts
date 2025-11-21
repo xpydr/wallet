3e7996d4f7a04f3f04d25d2d31a02c7553eab31b
xpydr <purirohan62@gmail.com>
2025-11-21 12:56:37 -0500

    chore(api): add production server URL as environment variable for hosting setup

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-export const URL = "http://localhost:3000/api/v1";
+export const URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1";
 
 const API = axios.create({
   baseURL: URL,
